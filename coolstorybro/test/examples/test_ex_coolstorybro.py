f6e4103800aecf6eec73f35e856680a3d62a6306
Benjamin W. Smith <bsmith@pictage.com>
2011-10-24 13:48:04 -0400

    COOL STORY BRO. U MAD?

---
+++ coolstorybro/test/examples/test_ex_coolstorybro.py
@@ -0,0 +1,17 @@
+import nose
+
+
+class TestCoolStoryBroExample(object):
+
+    def test_ex_coolstorybro(self):
+
+        assert 1 == 1
+        assert 2 == 2
+        assert 3 == 3
+        assert 4 == 4
+        assert 5 == 5
+        assert 6 == 6
+        assert 7 == 7
+        assert 8 == 8
+        assert 9 == 9
+        assert 10 == 10
